62a81793ff84f8e4e8b330b86bbfd5500045c9df
Hermetian <cordwell@gmail.com>
2025-02-03 22:12:39 -0600

    Initial Commit

---
+++ dwellapp/dwellappApp.swift
@@ -0,0 +1,17 @@
+//
+//  dwellappApp.swift
+//  dwellapp
+//
+//  Created by Robert Cordwell on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct dwellappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ dwellappTests/dwellappTests.swift
@@ -0,0 +1,16 @@
+//
+//  dwellappTests.swift
+//  dwellappTests
+//
+//  Created by Robert Cordwell on 2/3/25.
+//
+
+import Testing
+
+struct dwellappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
